9a7f97de5f6f2cb69e2205fd52dbc56af06be2a2
Adrien Thierry <adrienthierry2@gmail.com>
2016-05-09 17:52:18 -0400

    First working version : simple network that tries to make the output equal to the input

--- src/main.cpp
+++ src/main.cpp
@@ -5,11 +5,12 @@
 #include <cmath>
 #include <vector>
 
-#define MAX_ERROR 0.01
-#define SPEED 1
+#define MAX_ERROR 0.001
+#define MIN_GOOD 10
 
 struct Neuron {
 	std::vector<float> weights; // Weights of connexions with next layer
+	float bias;
 };
 
 struct Layer {
@@ -34,18 +35,21 @@ void init_network(struct Network* n, int numLayers, int numNeurons[]) {
 
 		// Iterate on neurons for current layer
 		for (int j = 0 ; j < numNeurons[i] ; j++) {
-			struct Neuron* n = new struct Neuron;
+			struct Neuron* neuron = new struct Neuron;
 
 			// Random weights with neurons of next layer
 			if (i < numLayers - 1) { // If there is a next layer
 
 				// Iterate on next layer neurons
 				for (int k = 0 ; k < numNeurons[i+1] ; k++) {
-					n->weights.push_back((float)rand() / (float)RAND_MAX);
+					neuron->weights.push_back((float)rand() / (float)RAND_MAX);
 				}
 			}
 
-			l->neurons.push_back(n);
+			// Random bias
+			neuron->bias = (float)rand() / (float)RAND_MAX;
+
+			l->neurons.push_back(neuron);
 		}
 
 		n->layers.push_back(l);
@@ -86,8 +90,8 @@ void print_weights(struct Network* n) {
 // @returns :
 //		- output
 //----------------------------------------------------------------------
-float sigmoid(float x) {
-	return 1 / (1 + exp(-x));
+float sigmoid(float x, float bias) {
+	return 1 / (1 + exp(-(x+bias)));
 }
 
 //----------------------------------------------------------------------
@@ -96,9 +100,10 @@ float sigmoid(float x) {
 //		- neural network
 //		- input values (same size as first layer)
 // @returns :
-//		- output values (same size as last layer)
+//		- all output values in network (first coord is layer, second is
+//		neuron output)
 //----------------------------------------------------------------------
-std::vector<float> front_propagation(struct Network *n, std::vector<float> *input) {
+std::vector<std::vector<float> > front_propagation(struct Network *n, std::vector<float> *input) {
 	
 	std::vector<std::vector<float> > internalResults;
 
@@ -126,7 +131,7 @@ std::vector<float> front_propagation(struct Network *n, std::vector<float> *inpu
 		}
 	}
 	for (unsigned int i = 0 ; i < currentOutputLayer->neurons.size() ; i++) {
-		internalResults.at(0).at(i) = sigmoid(internalResults.at(0).at(i));
+		internalResults.at(0).at(i) = sigmoid(internalResults.at(0).at(i), currentOutputLayer->neurons.at(i)->bias);
 	}
 
 	// Compute remaining internal results
@@ -143,36 +148,200 @@ std::vector<float> front_propagation(struct Network *n, std::vector<float> *inpu
 			}
 		}
 		for (unsigned int j = 0 ; j < currentOutputLayer->neurons.size() ; j++) {
-			internalResults.at(i).at(j) = sigmoid(internalResults.at(i).at(j));
+			internalResults.at(i).at(j) = sigmoid(internalResults.at(i).at(j), currentOutputLayer->neurons.at(j)->bias);
 		}
 	}
 
-	// Result output of last layer
-	return internalResults.back();
+	return internalResults;
+}
+
+//----------------------------------------------------------------------
+// Compute error
+// @params :
+//		- output values
+//		- desired output values
+// @returns :
+//		- error
+//----------------------------------------------------------------------
+float compute_error(std::vector<float> *output, std::vector<float> *desiredOutput) {
+	
+	float result = 0.0;
+
+	// Check that output and desiredOutput have same length
+	if (output->size() != desiredOutput->size()) {
+		printf("computeError ERROR : output and desiredOutput must have same size\n");
+		return -1;
+	}
+
+	for (unsigned int i = 0 ; i < output->size() ; i++) {
+		result += ((desiredOutput->at(i) - output->at(i)) * (desiredOutput->at(i) - output->at(i)));
+	}
+
+	return result;
+}
+
+//----------------------------------------------------------------------
+// Back propagation
+// @params :
+//		- neural network
+//		- internal outputs in network
+//		- input values
+//		- desired output
+//		- speed factor
+//----------------------------------------------------------------------
+void back_propagation(	struct Network *n, std::vector<std::vector<float> > *internalResults, 
+						std::vector<float> *inputs, std::vector<float> *desiredOutput, float speed) {
+	
+	std::vector<std::vector<float> > deltas;
+
+	// Initialize deltas
+	for (unsigned int i = 1 ; i < n->layers.size() ; i++) { // Iterate on layers
+		std::vector<float> currentDeltas;
+
+		struct Layer* l = n->layers.at(i);
+
+		// Iterate on neurons in layer
+		for (unsigned int j = 0 ; j < l->neurons.size() ; j++) {
+			currentDeltas.push_back(0.0);
+		}
+
+		deltas.push_back(currentDeltas);
+	}
+
+	// Compute deltas for last layer
+	std::vector<float> *lastLayerDeltas = &(deltas.back());
+	struct Layer *lastLayer = n->layers.back();
+	for (unsigned int i = 0 ; i < lastLayer->neurons.size() ; i++) { // Iterate on neurons
+		float currentDelta = 1.0;
+		currentDelta *= internalResults->back().at(i);
+		currentDelta *= (1.0 - internalResults->back().at(i));
+		currentDelta *= (internalResults->back().at(i) - desiredOutput->at(i));
+
+		lastLayerDeltas->at(i) = currentDelta;
+	}
+
+	// Compute deltas for remaining layers except first one
+	for (unsigned int i = n->layers.size() - 2 ; i > 0 ; i--) { // Iterate on layers
+		struct Layer* currentLayer = n->layers.at(i);
+		std::vector<float> *currentLayerDeltas = &(deltas.at(i-1));
+		std::vector<float> *nextLayerDeltas = &(deltas.at(i));
+
+		for (unsigned int j = 0 ; j < currentLayer->neurons.size() ; j++) {
+
+			struct Neuron* currentNeuron = currentLayer->neurons.at(j);
+
+			// Compute sum of weighted next layer deltas
+			float weightedSumNextLayerDeltas = 0.0;
+
+			for (unsigned int k = 0 ; k < nextLayerDeltas->size() ; k++) { // Iterate on next layer deltas
+				weightedSumNextLayerDeltas += (nextLayerDeltas->at(k) * currentNeuron->weights.at(k));			
+			}
+
+			float currentDelta = 1.0;
+			currentDelta *= internalResults->at(i-1).at(j);
+			currentDelta *= (1.0 - internalResults->at(i-1).at(j));
+			currentDelta *= weightedSumNextLayerDeltas;
+
+			currentLayerDeltas->at(j) = currentDelta;
+		}
+	}
+
+	// Update weights for first layer
+	struct Layer* firstLayer = n->layers.at(0);
+	for (unsigned int i = 0 ; i < firstLayer->neurons.size() ; i++) {
+
+		for (unsigned int j = 0 ; j < firstLayer->neurons.at(i)->weights.size() ; j++) {
+			float deltaW = -speed * deltas.at(0).at(j) * inputs->at(i);
+			firstLayer->neurons.at(i)->weights.at(j) += deltaW;
+		}
+	}
+
+	// Update weights for other layers (except last one)
+	for (unsigned int i = 1 ; i < n->layers.size() - 1 ; i++) {
+		
+		struct Layer* currentLayer = n->layers.at(i);
+		for (unsigned int j = 0 ; j < currentLayer->neurons.size() ; j++) {
+
+			for (unsigned int k = 0 ; k < currentLayer->neurons.at(j)->weights.size() ; k++) {
+				float deltaW = -speed * deltas.at(i).at(k) * internalResults->at(i-1).at(k);
+				currentLayer->neurons.at(j)->weights.at(k) += deltaW;
+			}
+		}
+	}
+
+	// Update bias
+	for (unsigned int i = 1 ; i < n->layers.size() ; i++) {
+		struct Layer* currentLayer = n->layers.at(i);
+
+		for (unsigned int j = 0 ; j < currentLayer->neurons.size() ; j++) {
+			float deltaBias = -speed * deltas.at(i-1).at(j);
+			currentLayer->neurons.at(j)->bias += deltaBias;
+		}
+	}
+}
+
+//----------------------------------------------------------------------
+// Print float vector
+// @params :
+//		- float vector
+//----------------------------------------------------------------------
+void print_float_vector(std::vector<float> *vector) {
+	printf("[");
+
+	for (unsigned int i = 0 ; i < vector->size() ; i++) {
+		printf("%f", vector->at(i));
+		if (i < vector->size() - 1)
+			printf(" ");
+	}
+
+	printf("]\n");
 }
 
 int main (int argc, char *argv[]) {
 
 	struct Network n;
-	std::vector<float> input;
 
 	srand(time(NULL));
 
 	// Initialize network
-	int numNeurons[3] = {2, 3, 2};
+	int numNeurons[3] = {1, 1, 1};
 	init_network(&n, 3, numNeurons);
 
-	print_weights(&n);
+	// Training loop
+	float error;
+	int numIterations = 0;
+	int numGood = 0;
+	do {
+		std::vector<float> input;
+		input.push_back((float)rand() / (float)RAND_MAX);
 
-	input.push_back(0.6);
-	input.push_back(0.8);
+		std::vector<std::vector<float> > result = front_propagation(&n, &input);
+		error = compute_error(&(result.back()), &input);
+		//printf("Error = %f\n", error);
+		back_propagation(&n, &result, &input, &input, 0.01);
+		numIterations++;
 
-	std::vector<float> result = front_propagation(&n, &input);
+		//print_float_vector(&(input));
+		//print_float_vector(&(result.back()));
 
-	printf("\n\n");
+		if (error < MAX_ERROR)
+			numGood++;
+		else
+			numGood = 0;
 
-	for (unsigned int i = 0 ; i < result.size() ; i++) {
-		printf("%f\n", result.at(i));
+	} while (numGood < MIN_GOOD);
+
+	printf("\nNumber of iterations : %d\n", numIterations);
+	
+	// Let user test the trained network
+	float input;
+	while(1) {
+		scanf("%f", &input);
+		std::vector<float> inputVector;
+		inputVector.push_back(input);
+
+		std::vector<std::vector<float> > result = front_propagation(&n, &inputVector);
+		print_float_vector(&(result.back()));
 	}
 
 	return 0;
