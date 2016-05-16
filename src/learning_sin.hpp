cc84a012a834ad15e1a34bbec9d360dda0a5daa6
Adrien Thierry <adrienthierry2@gmail.com>
2016-05-16 15:21:45 -0400

    Changed x values for sin learning

--- src/learning_sin.hpp
+++ src/learning_sin.hpp
@@ -16,8 +16,8 @@
 #define MAX_ERROR 0.001
 #define MIN_GOOD 100 // Number of successive times the network must achieve an error below the max error
 #define TOTAL_NUM_VALUES 10000 // Total number of values in data set (training + validation + test)
-#define MIN_X 0.0
-#define MAX_X 10
+#define MIN_X -5.0
+#define MAX_X 5.0
 
 void learning_sin();
 
